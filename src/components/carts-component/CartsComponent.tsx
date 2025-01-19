d4ed0712c11a1de727bc5511a2575b36355396de
Leskiv Andrii <58334364+AndriiLeskiv@users.noreply.github.com>
2025-01-19 18:30:41 +0200

    Homework 3-2(1)

---
+++ src/components/carts-component/CartsComponent.tsx
@@ -0,0 +1,28 @@
+import {useParams} from "react-router-dom";
+import {useEffect, useState} from "react";
+import {ICart} from "../../models/ICart.ts";
+import {ICartResponseModel} from "../../models/ICartResponseModel.ts";
+import {cartService} from "../../services/api.service.ts";
+import {CartComponent} from "../cart-component/CartComponent.tsx";
+
+export const CartsComponent = () => {
+
+    const {id} = useParams();
+
+    const [carts, setCarts] = useState<ICart[]>([]);
+    useEffect(() => {
+        if (id){
+            cartService.getCartsOfUser(id)
+                .then(({carts}:ICartResponseModel) => setCarts(carts));
+        }
+    },[id]);
+    return (
+        <div>
+            {
+                carts.map((cart: ICart) =>(
+                    <CartComponent key={cart.id} cart={cart}/>
+                ))
+            }
+        </div>
+    );
+};
