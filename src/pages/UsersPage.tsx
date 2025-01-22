c9ceae8d77103d2c0c9382219a9982ffab02d908
AndriiLeskiv <leskivandrii21@gmail.com>
2025-01-22 17:07:17 +0200

    fixed hw3-3

--- src/pages/UsersPage.tsx
+++ src/pages/UsersPage.tsx
@@ -1,11 +1,8 @@
-import {Outlet} from "react-router-dom";
 import {UsersComponent} from "../components/users/UsersComponent.tsx";
 
 export const UsersPage = () => {
     return (
         <div>
-            <Outlet/>
-            <hr/>
             <UsersComponent/>
         </div>
     );
